0b5c9e40a8e7ac9549676a555442fcca7cdf81ab
pupil <pupil@ivkhk.ee>
2020-09-24 10:44:38 +0300

    first vommit

--- src/jptvr19alphabet/App.java
+++ src/jptvr19alphabet/App.java
@@ -1,17 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package jptvr19alphabet;
 
-/**
- *
- * @author pupil
- */
-public class App {
+import java.util.Scanner;
+
+class App {
     public void run() {
-        System.out.println("----ABC----");
-}
-    
+        /*
+         * Создаем массив char c алфавитом abc[33]; +
+         * Просим пользователя ввести строку str +
+         * Проходим по массиву abc и ищем индекс вхождения этого символа в str.
+         * Если возвращается -1, то такого символа нет -> записываем его в массив
+         * нехватающих символов notAbc[33]; +
+         * если -1 не встречался, то текст содержит все буквы алфавита.
+         * если -1 сработал, то выводим набор символов, которые отсутствуют в тексте.
+         */
+        Scanner scanner = new Scanner(System.in);
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        char abc[] =  alphabet.toCharArray();
+        char notAbc[] = new char[33];
+
+        boolean isAllLetters = true;
+
+        System.out.print("Введите строку: ");
+        String str = scanner.nextLine();
+
+        for (int i = 0; i < abc.length; i++){
+            int index = str.indexOf(abc[i]);
+            if (index == -1) {
+                notAbc[i] = abc[i]; isAllLetters = false;
+            }
+        }
+        if (!isAllLetters){
+            System.out.println("В строке не все буквы, нехватает: " + new String(notAbc));
+        } else {
+            System.out.println("В строке все буквы");
+        }
+    }
 }
